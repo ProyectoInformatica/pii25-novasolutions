ee865cb69f657c151afe0b5933d3213fb85a2883
Sebasmsz <sebastianmarcanosanchez@gmail.com>
2026-05-17 18:08:07 +0200

    Actualizacion de umbrales

--- src/view/Etiquetas_Sensores.py
+++ src/view/Etiquetas_Sensores.py
@@ -4,13 +4,13 @@ _COLOR_SIN_DATOS = "#888888"
 
 def interpretar_calidad_aire(valor: float) -> tuple[str, str]:
 
-    if valor < 400:
+    if valor < 800:
         return "Buena",            "#00ff00"
-    elif valor < 600:
+    elif valor < 1400:
         return "Regular",          "#aaff00"
-    elif valor < 900:
+    elif valor < 2200:
         return "Mala",             "#ffaa00"
-    elif valor < 1300:
+    elif valor < 3000:
         return "Muy mala",         "#ff6600"
     else:
         return "Muy desfavorable", "#ff0000"
